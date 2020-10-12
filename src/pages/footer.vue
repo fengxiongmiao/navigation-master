3ab07c1962a6b7189d0440ef9d054f323b80cd9d
tao <startoffly>
2020-10-12 22:26:14 +0800

    主页样式调整与footer元素

--- src/pages/footer.vue
+++ src/pages/footer.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
-    foot
+  <div class="xinmove-footer">
+    <div class="footer-container">
+      <div class="footer-logo">
+        © 2020 xinmove.com<a href="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_blank" class="beian-a">浙ICP备17052134号-1</a>
+        <span id='cnzz_stat_icon_1279294755'></span>
+<!--        <script src='http://s9.cnzz.com/z_stat.php?id=1279294755&show=pic' type='text/javascript'></script>-->
+        <a  href="Mailto:startbing@foxmail.com" title="邮件：startbing@foxmail.com" class="bga"><i class="fa fa-envelope"></i> 发送邮件</a>
+        <!-- <a  href="https://github.com/startoffly" title="github：BEWINDOWEB" target="_blank" class="bga"><i class="fa fa-github"></i> github</a> -->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "",
+      mounted () {
+        const script = document.createElement('script')
+        script.src = 'https://s95.cnzz.com/z_stat.php?id=1279294755&show=pic'
+        script.language = 'JavaScript'
+        document.body.appendChild(script)
+      },
+      watch: {
+        '$route' () {
+          if (window._czc) {
+            let location = window.location
+            let contentUrl = location.pathname + location.hash
+            let refererUrl = '/'
+            window._czc.push(['_trackPageview', contentUrl, refererUrl])
+          }
+        }
+      }
     }
 </script>
 
 <style scoped>
   /* ============== footer ========= */
   .xinmove-footer{
+    /*padding: 20px 0px 20px 0px;*/
     width:100%;
     height:20px;
     background-color:#242424;
